1b2046b907862e74c0a4ab38b94957f86c50685a
Mattea29 <mwhitlow@smith.edu>
2023-12-06 13:19:53 -0500

    idk what changed here?

--- Enemy.java
+++ Enemy.java
@@ -4,4 +4,6 @@ public class Enemy extends Person {
     public void getWeapon() {
         // method which allows us to get the weapon type of an enemy, which will be useful for attack info and tracking health of player
     }
+
+
 }
